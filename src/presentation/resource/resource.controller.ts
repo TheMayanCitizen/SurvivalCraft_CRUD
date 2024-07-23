1a8bb5acc308a6e470c7c9b551af889f63380f5b
Alejandro Aguilar <98495755+TheMayanCitizen@users.noreply.github.com>
2024-07-22 17:17:19 -0700

    other files added

---
+++ src/presentation/resource/resource.controller.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+import { CreateResourceDTO, CustomError } from "../../domain";
+import { ResourceService } from "../services/resource.service";
+
+export class ResourceController {
+  constructor(private readonly resourceService: ResourceService) {}
+
+  private handleError = (error: unknown, res: Response) => {
+    if (error instanceof CustomError) {
+      return res.status(error.statusCode).json({ message: error.message });
+    }
+
+    console.log(error);
+    return res.status(500).json({ message: "Something went very wrong! 🧨" });
+  };
+
+  createNewResource = async (req: Request, res: Response) => {
+    const [error, createResourceDTO] = CreateResourceDTO.create(req.body);
+    if (error) return res.status(422).json({ message: error });
+
+    this.resourceService
+      .createResource(createResourceDTO!)
+      .then((resource) => res.status(201).json(resource))
+      .catch((error) => this.handleError(error, res));
+  };
+}
---
+++ src/presentation/resource/resource.routes.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import { ResourceController } from "./resource.controller";
+import { ResourceService } from "../services/resource.service";
+
+export class ResourceRoutes {
+  static get routes(): Router {
+    const router = Router();
+
+    const resourceService = new ResourceService();
+    const controller = new ResourceController(resourceService);
+
+    router.post("/create-new-resource", controller.createNewResource);
+
+    return router;
+  }
+}
